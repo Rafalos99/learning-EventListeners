7c29253fee0d3b869c08403f8f2cc8a3e7f24a82
Rafsanzani Ludhi <rafsanzaniludhi@gmail.com>
2022-11-01 16:19:42 +0000

    Trying to add event listeners for colors

---
+++ script.js
@@ -0,0 +1,30 @@
+const grandparent = document.querySelector(".grandparent")
+const parent = document.querySelector(".parent")
+const child = document.querySelector(".child")
+
+// First setting up a "click" event listener on the grandparent 
+
+// // (type of event we want to listen for, in our case, its 'click'    then  a 
+// callback: a function that runs when we do whatever the event is, generally called event object, here I used an arrow function e=>)
+// Console.log e.target as thats the thing the event happens on. Changed to Grandparent 1
+
+grandparent.addEventListener("click", printRed)
+
+parent.addEventListener("click", printBlue)
+
+child.addEventListener("click", printGreen)
+
+// Naming some functions to call down below when clicking on different colors
+
+function printRed() {
+    console.log("I am Red!!!")
+}
+
+function printBlue() {
+    console.log("I am Bluee!!!")
+}
+
+function printGreen() {
+    console.log("I am Greeeen!!!")
+}
+
